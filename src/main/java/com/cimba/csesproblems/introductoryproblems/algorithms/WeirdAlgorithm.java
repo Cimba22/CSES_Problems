61c011cc297bf23657b7d504ee636f90866059b9
Oleksandra Mala <oleksandra.s.mala@gmail.com>
2022-04-02 00:28:28 +0200

    First task

--- src/main/java/com/cimba/csesproblems/introductoryproblems/algorithms/WeirdAlgorithm.java
+++ src/main/java/com/cimba/csesproblems/introductoryproblems/algorithms/WeirdAlgorithm.java
@@ -1,4 +1,27 @@
 package com.cimba.csesproblems.introductoryproblems.algorithms;
 
 public class WeirdAlgorithm {
+
+    public void algorithm(long n){
+
+        //long start = System.nanoTime();
+
+        if (n == 1){
+            System.out.print(1 + " ");
+            return;
+        }else {
+            System.out.print(n + " ");
+            if(n % 2 == 0){
+                algorithm(n/2);
+            }else {
+                algorithm(n * 3 + 1);
+            }
+        }
+
+        //TODO time counter
+        //long finish = System.nanoTime();
+        //System.out.println("\n");
+        //System.out.println(finish - start);
+        //return finish - start;
+    }
 }
