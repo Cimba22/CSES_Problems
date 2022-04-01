bd0b559b3c2eab38c57309e392d08cc716c6e425
Oleksandra Mala <oleksandra.s.mala@gmail.com>
2022-04-02 00:30:25 +0200

    Demonstration class of algorithms

--- src/main/java/com/cimba/csesproblems/introductoryproblems/Demo.java
+++ src/main/java/com/cimba/csesproblems/introductoryproblems/Demo.java
@@ -1,7 +1,20 @@
 package com.cimba.csesproblems.introductoryproblems;
 
+import com.cimba.csesproblems.introductoryproblems.algorithms.WeirdAlgorithm;
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        WeirdAlgorithm weirdAlgorithm = new WeirdAlgorithm();
+        weirdAlgorithm.algorithm(3);
+
+
+        //TODO Rounding for time counting
+       // DecimalFormat df = new DecimalFormat("#.###");
+       // df.setRoundingMode(RoundingMode.CEILING);
+       // String time = df.format((long) weirdAlgorithm.algorithm(3) * Math.pow(10, -9)) + " secs";
+        //System.out.format("%15s\n",time);
     }
 }
